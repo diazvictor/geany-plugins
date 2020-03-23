599195dd79eac1f35208325a327e877ddbf2b63f
diazvictor <diaz.victor@openmailbox.org>
2020-03-23 03:08:57 -0400

    reduced code, some fixes are still missing, but for the moment the plugin is working 🥳🥳🎉🎉🎉

--- geany-docblockr/docblokr.lua
+++ geany-docblockr/docblokr.lua
@@ -1,26 +1,16 @@
 --[[
  @package   GeanyPluginsLua
  @filename  geany-docblokr/docblokr.lua
- @version   1.0
- @author    Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
- @date      11.07.2018 23:50:09 -04
+ @version   2.0
+ @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ @date      23.03.2020 01:35:35 -04
 ]]--
 
-function docblockr()
-	local selection = geany.selection()
-	local valor = selection:match("%s(%a+)")
-	if ( selection:match("function") ~= nil) then
-		if ( selection == valor ) then
-			geany.selection("--[[\n [description of "..valor.."]\n]]--\n"..selection)
-		elseif ( selection ~= valor ) then
-			local valor = selection:match("%s(%a+.%a+)")
-			geany.selection("--[[\n [description of "..valor.."]\n]]--\n"..selection)
-		end
-	else
-		geany.message("sdss")
-	end
+local name = geany.selection()
+local var = " "..name:match("%s(%a+.%a+)").." [description]\n"
+
+for k,v in name:gsub(".*%(", ""):gmatch("(%w+)") do
+	var = var .. string.format(" @param {[type]} %s [description]\n", k)
 end
 
-if geany.fileinfo().ext == ".lua" then
-	docblockr()
-end
+geany.selection("--[[\n"..var.." @return {[type]}\n]]--\n"..name)
