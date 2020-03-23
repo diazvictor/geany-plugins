4aec8e6e000ba050b6df7f56b85eb6ba48e779e7
diazvictor <diaz.victor@openmailbox.org>
2020-03-23 03:39:29 -0400

    Add screenshot, minimal changes in spacing and credits to contributors

---
+++ geany-docblockr/README.md
@@ -0,0 +1,25 @@
+# Geany Dockblockr
+
+> DocBlockr is a package for Geany which is designed to make writing documentation faster and easier.
+
+The package currently supports the following languages -
+
+* Lua
+
+![screenshot](screenshot.png "Geany Dockblockr")
+
+### Requirements
+
+[geanylua](https://github.com/geany/geany-plugins/tree/master/geanylua)
+
+### License
+
+GPL-3 <https://www.gnu.org/licenses/gpl-3.0.html>
+
+### Note
+
+Based on Dockblockr from SublimeText
+
+#### Version
+
+2.0
--- geany-docblockr/docblokr.lua
+++ geany-docblockr/docblokr.lua
@@ -1,16 +1,18 @@
 --[[
- @package   GeanyPluginsLua
- @filename  geany-docblokr/docblokr.lua
- @version   2.0
- @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
- @date      23.03.2020 01:35:35 -04
+ @package      GeanyPluginsLua
+ @filename     geany-docblokr/docblokr.lua
+ @version      2.0
+ @author       Díaz Urbaneja Víctor Eduardo Diex (diazvictor) <victor.vector008@gmail.com>
+ @contributor  Díaz  Víctor  aka  (Máster Vitronic) <vitronic2@gmail.com> <mastervitronic@vitronic.com.ve>
+ @contributor  Telegram Marco Trosi @marcotrosi
+ @date         23.03.2020 01:35:35 -04
 ]]--
 
-local name = geany.selection()
-local var = " "..name:match("%s(%a+.%a+)").." [description]\n"
+local selection = geany.selection()
+local value = " " .. selection:match("%s(%a+.%a+)").." [description]\n"
 
-for k,v in name:gsub(".*%(", ""):gmatch("(%w+)") do
-	var = var .. string.format(" @param {[type]} %s [description]\n", k)
+for k,v in selection:gsub(".*%(", ""):gmatch("(%w+)") do
+	value = value .. string.format(" @param {[type]} %s [description]\n", k)
 end
 
-geany.selection("--[[\n"..var.." @return {[type]}\n]]--\n"..name)
+geany.selection("--[[\n".. value .." @return {[type]}\n]]--\n" .. selection)
